d8f810cdb04af9df9d3f016e8541e840ee017356
Vikram Jain <vikramjainiit@gmail.com>
2023-09-18 17:39:13 -0700

    Setting up the protocol layer codebase

---
+++ src/main/scala/protocol/Common.scala
@@ -0,0 +1,15 @@
+package ucie.tlfront
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.BundleLiterals._
+import scala.math._
+
+// Protocol layer and TileLink parameters
+
+case class ProtocolLayerParams (
+    val tlBaseAddress: BigInt,
+    val configBaseAddress: BigInt
+) {
+    val beatByte: Int = 32
+}
