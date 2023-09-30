1a9fc99821cae21be51be48229a75128e3ae8602
Viansa Schmulbach <ansa@berkeley.edu>
2023-09-29 20:42:38 -0700

    style fix

--- src/main/scala/logphy/LogicalPhy.scala
+++ src/main/scala/logphy/LogicalPhy.scala
@@ -3,7 +3,12 @@ package edu.berkeley.cs.ucie.digital.logphy
 import chisel3._
 import edu.berkeley.cs.ucie.digital.interfaces._
 
-class LogicalPhy(lanes: Int = 16, serializerRatio: Int = 16, width: Int, sbWidth: Int) extends Module {
+class LogicalPhy(
+    lanes: Int = 16,
+    serializerRatio: Int = 16,
+    width: Int,
+    sbWidth: Int,
+) extends Module {
   val io = IO(new Bundle {
     val rdi = Flipped(new Rdi(width, sbWidth))
     val mbAfe = new MainbandAfeIo(lanes, serializerRatio)
