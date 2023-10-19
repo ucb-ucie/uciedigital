88190fb4f4028ab5f4ecea8ad7bd50c8c5079257
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2023-10-18 22:38:59 -0700

    get d2d dummy module to elaborate

--- src/test/scala/D2DDummyLoopback.scala
+++ src/test/scala/D2DDummyLoopback.scala
@@ -2,7 +2,9 @@ package edu.berkeley.cs.ucie.digital
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental.FlatIO
 import interfaces._
+
 import chisel3.stage.ChiselStage
 
 // LatencyPipe from rocket-chip
@@ -31,10 +33,10 @@ class D2DDummyLoopback(latency: Int = 8) extends Module {
   // 64 bit wide data bus, 8 bit wide side channel bus
   val fdiParams = FdiParams(width=8, dllpWidth=8, sbWidth=8)
 
-  val io = IO(Flipped(new Fdi(fdiParams))) // implicit module clock = lclk
+  val io = FlatIO(Flipped(new Fdi(fdiParams))) // implicit module clock = lclk
 
   when (io.lpData.valid) {
-    // For now, the protocol layer must asssert lpData.valid and lpData.irdy together
+    // For now, the protocol layer must assert lpData.valid and lpData.irdy together
     chisel3.assert(io.lpData.irdy, "lpData.valid was asserted without lpData.irdy")
   }
 
@@ -54,8 +56,34 @@ class D2DDummyLoopback(latency: Int = 8) extends Module {
   // Tieoffs
   io.plProtocol := Protocol.streaming
   io.plProtocolFlitFormat := FlitFormat.raw
+  io.plProtocolValid := true.B // TODO: need to model this signal changing from false to true upon training completion
+  io.plInbandPres := false.B // TODO: this depends on the FSM state
+  io.plCerror := false.B
+  io.plFlitCancel := false.B
+  io.plRxActiveReq := false.B // TODO: this signal indicates when the protocol layer can receive bits
+  io.plClkReq := true.B // we don't support dynamic clock gating
+  io.plRetimerCrd := false.B // this is an optional signal anyways
+  io.plStream.protoStack := ProtoStack.stack0
+  io.plStream.protoType := ProtoStreamType.Stream
+  io.plNfError := false.B // TODO: we want to be able to inject errors from the adapter to protocol layer
+  io.plDllp.valid := false.B // we don't use the DLLP interface due to no PCIe support
+  io.plDllp.bits := 0.U
+  io.plDllpOfc := false.B
+  io.plPhyInL1 := false.B // TODO: need to use these if the D2D adapter handles multiple power states
+  io.plPhyInL2 := false.B
+  io.plConfigCredit := false.B // TODO: need to handle sideband packets
+  io.plConfig.valid := false.B
+  io.plConfig.bits := 0.U
+  io.plStallReq := false.B // TODO: need to model D2D adapter requesting protocol layer to flush flits
+  io.plWakeAck := RegNext(io.lpWakeReq)
+  io.plLinkWidth := PhyWidth.width64 // TODO: this is a PHY-level parameter
+  io.plSpeedMode := SpeedMode.speed4 // TODO: this is a runtime PHY knob
+  io.plPhyInRecenter := false.B // TODO: this is part of the FDI state machine
+  io.plTrainError := false.B // TODO: need to model fatal errors of the PHY
+  io.plError := false.B // TODO: need to model framing errors
+  io.plStateStatus := PhyState.active // TODO: this is part of the FDI state machine
 }
 
 object D2DDummyLoopbackMain extends App {
-  new ChiselStage().emitVerilog(new D2DDummyLoopback())
+  new ChiselStage().emitVerilog(new D2DDummyLoopback(), args=Array("--target-dir", "vlog"))
 }
---
+++ src/test/scala/D2DTest.scala
@@ -0,0 +1,12 @@
+package edu.berkeley.cs.ucie.digital
+
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+
+class D2DTest extends AnyFreeSpec with ChiselScalatestTester {
+  "D2D Dummy Loopback module should elaborate" in {
+    test(new D2DDummyLoopback())  { c =>
+      c.clock.step()
+    }
+  }
+}
