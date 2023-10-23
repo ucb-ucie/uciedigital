6a369443c3781c61109d4fc275ebfd1a35babb06
ronitnag04 <ronitnag04@gmail.com>
2023-10-23 02:06:54 -0700

    Added Reset Logic

--- src/main/scala/d2dadapter/CRCGenerator.scala
+++ src/main/scala/d2dadapter/CRCGenerator.scala
@@ -6,9 +6,11 @@ import chisel3.util._
 class CRCGenerator(width: Int) extends Module {
     val io = IO(new Bundle {
         val data_in = Input(UInt(width.W))      // Accepts next word of data from client
-
         val data_val = Input(Bool())            // Signal from client that data at data_in is valid and can be processed
+
         // val clk = Input(Clock())             // synchronous clock signal
+        val rst = Input(Bool())                 // reset CRC generator and registers
+
 
         val data_rdy = Output(Bool())           // Signal to client if generator is ready to accept another word of data
 
@@ -23,8 +25,27 @@ class CRCGenerator(width: Int) extends Module {
     val CRC_Val = RegInit(Bool(), 1.U)
     val Data_Rdy = RegInit(Bool(), 1.U)
 
-    // CRC Calculating registers
+    // CRC calculating registers
     val step = RegInit(UInt(log2Ceil(width).W), 0.U)
+
+
+    // Reset logic
+    when (io.rst) {
+        // Reset output data registers
+        CRC0 := 0.U
+        CRC1 := 0.U
+        CRC_Val := 1.U
+        Data_Rdy := 0.U
+
+        // Propogate output data register
+        io.crc0_out := CRC0
+        io.crc1_out := CRC1
+        io.crc_val := CRC_Val
+        io.data_rdy := Data_Rdy
+
+        // Reset CRC calculating registers
+        step := 0.U
+    }
 
 
 
