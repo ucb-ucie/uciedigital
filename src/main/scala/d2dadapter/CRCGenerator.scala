bf6287152e3f3daa425f1a28304b91fa1e805c8c
ronitnag04 <ronitnag04@gmail.com>
2023-10-23 01:53:59 -0700

    Wrote initial module structure

---
+++ src/main/scala/d2dadapter/CRCGenerator.scala
@@ -0,0 +1,33 @@
+package edu.berkeley.cs.ucie.digital.d2dadapter
+
+import chisel3._
+import chisel3.util._
+
+class CRCGenerator(width: Int) extends Module {
+    val io = IO(new Bundle {
+        val data_in = Input(UInt(width.W))      // Accepts next word of data from client
+
+        val data_val = Input(Bool())            // Signal from client that data at data_in is valid and can be processed
+        // val clk = Input(Clock())             // synchronous clock signal
+
+        val data_rdy = Output(Bool())           // Signal to client if generator is ready to accept another word of data
+
+        val crc0_out = Output(UInt(8.W))        // CRC 0 Byte output 
+        val crc1_out = Output(UInt(8.W))        // CRC 1 Byte output
+        val crc_val = Output(Bool())            // Signal to client that data on crc0_out and crc1_out are valid
+    })
+
+    // Output data registers
+    val CRC0 = RegInit(UInt(8.W), 0.U)
+    val CRC1 = RegInit(UInt(8.W), 0.U)
+    val CRC_Val = RegInit(Bool(), 1.U)
+    val Data_Rdy = RegInit(Bool(), 1.U)
+
+    // CRC Calculating registers
+    val step = RegInit(UInt(log2Ceil(width).W), 0.U)
+
+
+
+
+}
+
