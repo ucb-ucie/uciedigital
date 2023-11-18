e881300d56bbfd8fe2c505acea06d9ba9a9ebe94
Ice Unjitwattana <pawat.unj@gmail.com>
2023-11-17 22:29:37 -0800

    scrambler reformatted

--- src/main/scala/Scrambler.scala
+++ src/main/scala/Scrambler.scala
@@ -2,15 +2,12 @@ package edu.berkeley.cs.ucie.digital
 package interfaces
 
 import chisel3._
-import chisel3.util._
 import chisel3.util.random._
 
-// 
-
-class Scrambler (
-  afeParams: AfeParams,
-  width: Int,
-  seed: BigInt
+class Scrambler(
+    afeParams: AfeParams,
+    width: Int,
+    seed: BigInt,
 ) extends Module {
   val io = IO(new Bundle {
     val data_in = Input(UInt(afeParams.mbSerializerRatio.W))
@@ -18,28 +15,47 @@ class Scrambler (
     val seed = Input(UInt(23.W))
     val data_out = Output(UInt(afeParams.mbSerializerRatio.W))
   })
-  val LFSR = Module(new FibonacciLFSR(23, Set(23,21,18,15,7,2,1), Some(seed), XOR, width, false))
+  val LFSR = Module(
+    new FibonacciLFSR(
+      23,
+      Set(23, 21, 18, 15, 7, 2, 1),
+      Some(seed),
+      XOR,
+      width,
+      false,
+    ),
+  )
   LFSR.io.increment := io.valid
   LFSR.io.seed.bits := VecInit(io.seed.asBools)
   LFSR.io.seed.valid := (reset.asBool)
   val LFSR_result = LFSR.io.out
-  //printf(cf"$LFSR_result.asUInt")
+  // printf(cf"$LFSR_result.asUInt")
   io.data_out := LFSR_result.asUInt ^ io.data_in
 }
 
-class UCIeScrambler (
-  afeParams: AfeParams,
-  width: Int, 
-  numLanes: Int
+class UCIeScrambler(
+    afeParams: AfeParams,
+    width: Int,
+    numLanes: Int,
 ) extends Module {
   val io = IO(new Bundle {
     val data_in = Input(Vec(numLanes, UInt(afeParams.mbSerializerRatio.W)))
     val valid = Input(Bool())
     val data_out = Output(Vec(numLanes, UInt(afeParams.mbSerializerRatio.W)))
   })
-  val UCIe_seeds = List("1dbfbc", "0607bb", "1ec760", "18c0db", "010f12", "19cfc9", "0277ce", "1bb807")
+  val UCIe_seeds = List(
+    "1dbfbc",
+    "0607bb",
+    "1ec760",
+    "18c0db",
+    "010f12",
+    "19cfc9",
+    "0277ce",
+    "1bb807",
+  )
   val seeds = (for (i <- 0 until numLanes) yield UCIe_seeds(i % 8)).toList
-  val scramblers = seeds.map(seed => Module(new Scrambler(afeParams, width, BigInt(seed, 16))))
+  val scramblers =
+    seeds.map(seed => Module(new Scrambler(afeParams, width, BigInt(seed, 16))))
   for (i <- 0 until scramblers.length) {
     scramblers(i).io.data_in := io.data_in(i)
     scramblers(i).io.valid := io.valid
@@ -49,4 +65,3 @@ class UCIeScrambler (
     io.data_out(i) := scramblers(i).io.data_out
   }
 }
-
