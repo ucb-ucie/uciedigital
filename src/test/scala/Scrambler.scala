979c7fa1638e48d58199e9b32d08d8d006095702
Ice Unjitwattana <pawat.unj@gmail.com>
2023-11-29 10:19:28 -0800

    missing python test script

--- src/test/scala/Scrambler.scala
+++ src/test/scala/Scrambler.scala
@@ -25,7 +25,15 @@ class ScramblerTest extends AnyFunSpec with ChiselScalatestTester {
         c.io.data_out(1).expect(1103.U(16.W))
         c.io.data_out(2).expect(50263.U(16.W))
         c.io.data_out(3).expect(49245.U(16.W))
-
+        c.clock.step()
+        c.io.data_in(0).poke(203.U(16.W))
+        c.io.data_in(1).poke(176.U(16.W))
+        c.io.data_in(2).poke(21.U(16.W))
+        c.io.data_in(3).poke(5847.U(16.W))
+        c.io.data_out(0).expect(65321.U(16.W))
+        c.io.data_out(1).expect(56489.U(16.W))
+        c.io.data_out(2).expect(11245.U(16.W))
+        c.io.data_out(3).expect(57654.U(16.W))
       }
     }
   }
