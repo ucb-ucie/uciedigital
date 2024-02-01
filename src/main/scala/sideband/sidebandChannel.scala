2ae15dd09d43d4849d12522097399c2cc983ff0a
Vikram Jain <vikramjainiit@gmail.com>
2024-01-31 23:02:03 -0800

    Cleaned up sideband code

--- src/main/scala/sideband/sidebandChannel.scala
+++ src/main/scala/sideband/sidebandChannel.scala
@@ -1,52 +1,63 @@
-package ucie.sideband
+package edu.berkeley.cs.ucie.digital
+package sideband
 
 import chisel3._
 import chisel3.util._
 import chisel3.experimental._
 
+import interfaces._
+
 // import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.util._
 
-class D2DSidebandChannel (val myID: BigInt = BigInt(1), val params: SidebandParams) extends Module {
-    val io = IO(new D2DSidebandChannelIO(params))
+class D2DSidebandChannel(
+    val myID: BigInt = BigInt(1),
+    val sbParams: SidebandParams,
+    val fdiParams: FdiParams,
+) extends Module {
+  val io = IO(new D2DSidebandChannelIO(sbParams, fdiParams))
 
-    // Instantiate submodule
-    val upper_node = Module(new SidebandNode(params))
-    val switcher = Module(new sidebandSwitcher(myID, params))
-    val lower_node = Module(new SidebandNode(params))
+  // Instantiate submodule
+  val upper_node = Module(new SidebandNode(sbParams, fdiParams))
+  val switcher = Module(new sidebandSwitcher(myID, sbParams))
+  val lower_node = Module(new SidebandNode(sbParams, fdiParams))
 
-    // Connect outer signals
-    io.to_upper_layer <> upper_node.io.outer
-    io.to_lower_layer <> lower_node.io.outer
+  // Connect outer signals
+  io.to_upper_layer <> upper_node.io.outer
+  io.to_lower_layer <> lower_node.io.outer
 
-    // Connect two sidebandNodes and switcher
-    upper_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_above
-    upper_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_above
-    lower_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_below
-    lower_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_below
+  // Connect two sidebandNodes and switcher
+  upper_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_above
+  upper_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_above
+  lower_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_below
+  lower_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_below
 
-    // Connect inner signals
-    io.inner <> switcher.io.inner
+  // Connect inner signals
+  io.inner <> switcher.io.inner
 }
 
-class PHYSidebandChannel (val myID: BigInt = BigInt(2), val params: SidebandParams) extends Module {
-    val io = IO(new PHYSidebandChannelIO(params))
+class PHYSidebandChannel(
+    val myID: BigInt = BigInt(2),
+    val sbParams: SidebandParams,
+    val fdiParams: FdiParams,
+) extends Module {
+  val io = IO(new PHYSidebandChannelIO(sbParams, fdiParams))
 
-    // Instantiate submodule
-    val upper_node = Module(new SidebandNode(params))
-    val switcher = Module(new sidebandSwitcher(myID, params))
-    val lower_node = Module(new SidebandLinkNode(params))
+  // Instantiate submodule
+  val upper_node = Module(new SidebandNode(sbParams, fdiParams))
+  val switcher = Module(new sidebandSwitcher(myID, sbParams))
+  val lower_node = Module(new SidebandLinkNode(sbParams, fdiParams))
 
-    // Connect outer signals
-    io.to_upper_layer <> upper_node.io.outer
-    io.to_lower_layer <> lower_node.io.outer
+  // Connect outer signals
+  io.to_upper_layer <> upper_node.io.outer
+  io.to_lower_layer <> lower_node.io.outer
 
-    // Connect two sidebandNodes and switcher
-    upper_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_above
-    upper_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_above
-    lower_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_below
-    lower_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_below
+  // Connect two sidebandNodes and switcher
+  upper_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_above
+  upper_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_above
+  lower_node.io.inner.layer_to_node <> switcher.io.outer.layer_to_node_below
+  lower_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_below
 
-    // Connect inner signals
-    io.inner <> switcher.io.inner
+  // Connect inner signals
+  io.inner <> switcher.io.inner
 }
--- src/main/scala/sideband/sidebandIO.scala
+++ src/main/scala/sideband/sidebandIO.scala
@@ -1,152 +1,171 @@
-package ucie.sideband
+package edu.berkeley.cs.ucie.digital
+package sideband
 
 import chisel3._
 import chisel3.util._
 import chisel3.experimental._
 
+import interfaces._
+
 // import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.util._
 
-class D2DSidebandChannelIO (val params: SidebandParams) extends Bundle {
-    // connect to another sideband node in the layer above (protocol layer)
-    val to_upper_layer = new Bundle {
-        val tx = new Bundle {
-            val bits = Output(UInt(params.NC_width.W))
-            val valid = Output(Bool())
-            val credit = Input(Bool())
-        }
-        val rx = new Bundle {
-            val bits = Input(UInt(params.NC_width.W))
-            val valid = Input(Bool())
-            val credit = Output(Bool())
-        }
+class D2DSidebandChannelIO(
+    val sbParams: SidebandParams,
+    val fdiParams: FdiParams,
+) extends Bundle {
+  // connect to another sideband node in the layer above (protocol layer)
+  val to_upper_layer = new Bundle {
+    val tx = new Bundle {
+      val bits = Output(UInt(fdiParams.sbWidth.W))
+      val valid = Output(Bool())
+      val credit = Input(Bool())
     }
-    // connect to another sideband node in the layer below (physical layer)
-    val to_lower_layer = new Bundle {
-        val tx = new Bundle {
-            val bits = Output(UInt(params.NC_width.W))
-            val valid = Output(Bool())
-            val credit = Input(Bool())
-        }
-        val rx = new Bundle {
-            val bits = Input(UInt(params.NC_width.W))
-            val valid = Input(Bool())
-            val credit = Output(Bool())
-        }
+    val rx = new Bundle {
+      val bits = Input(UInt(fdiParams.sbWidth.W))
+      val valid = Input(Bool())
+      val credit = Output(Bool())
     }
-    // d2d layer drive these
-    val inner = Flipped(new SidebandSwitcherbundle(params))
+  }
+  // connect to another sideband node in the layer below (physical layer)
+  val to_lower_layer = new Bundle {
+    val tx = new Bundle {
+      val bits = Output(UInt(fdiParams.sbWidth.W))
+      val valid = Output(Bool())
+      val credit = Input(Bool())
+    }
+    val rx = new Bundle {
+      val bits = Input(UInt(fdiParams.sbWidth.W))
+      val valid = Input(Bool())
+      val credit = Output(Bool())
+    }
+  }
+  // d2d layer drive these
+  val inner = Flipped(new SidebandSwitcherbundle(sbParams))
 }
 
-class PHYSidebandChannelIO (val params: SidebandParams) extends Bundle {
-    // connect to another sideband node in the layer above (d2d layer)
-    val to_upper_layer = new Bundle {
-        val tx = new Bundle {
-            val bits = Output(UInt(params.NC_width.W))
-            val valid = Output(Bool())
-            val credit = Input(Bool())
-        }
-        val rx = new Bundle {
-            val bits = Input(UInt(params.NC_width.W))
-            val valid = Input(Bool())
-            val credit = Output(Bool())
-        }
+class PHYSidebandChannelIO(
+    val sbParams: SidebandParams,
+    val fdiParams: FdiParams,
+) extends Bundle {
+  // connect to another sideband node in the layer above (d2d layer)
+  val to_upper_layer = new Bundle {
+    val tx = new Bundle {
+      val bits = Output(UInt(fdiParams.sbWidth.W))
+      val valid = Output(Bool())
+      val credit = Input(Bool())
     }
-    // connect to another sideband node in the layer below (link layer)
-    val to_lower_layer = new Bundle {
-        val tx = new Bundle {
-            val bits = Output(UInt(params.NC_width.W))
-            val clock = Output(Bool())
-        }
-        val rx = new Bundle {
-            val bits = Input(UInt(params.NC_width.W))
-            val clock = Input(Bool())
-        }
+    val rx = new Bundle {
+      val bits = Input(UInt(fdiParams.sbWidth.W))
+      val valid = Input(Bool())
+      val credit = Output(Bool())
     }
-    // phy layer drive these
-    val inner = Flipped(new SidebandSwitcherbundle(params))
+  }
+  // connect to another sideband node in the layer below (link layer)
+  val to_lower_layer = new Bundle {
+    val tx = new Bundle {
+      val bits = Output(UInt(fdiParams.sbWidth.W))
+      val clock = Output(Bool())
+    }
+    val rx = new Bundle {
+      val bits = Input(UInt(fdiParams.sbWidth.W))
+      val clock = Input(Bool())
+    }
+  }
+  // phy layer drive these
+  val inner = Flipped(new SidebandSwitcherbundle(sbParams))
 }
 
 // IO for the RDI and FDI sideband
-class SidebandNodeIO (val params: SidebandParams) extends Bundle {
-    // layers drive these
-    val inner = new Bundle {
-        val layer_to_node = Flipped(Decoupled(UInt(params.MSG_width.W)))
-        // This signal overrides the tx.ready and takes up the priority reserved queue slot
-        // Should only be asserted high for access completion packets
-        val node_to_layer = Decoupled(UInt(params.MSG_width.W))
-    }
-    // connect these to another sideband node
-    val outer = new Bundle {
-        val tx = new Bundle {
-            val bits = Output(UInt(params.NC_width.W))
-            val valid = Output(Bool())
-            val credit = Input(Bool())
-        }
-        val rx = new Bundle {
-            val bits = Input(UInt(params.NC_width.W))
-            val valid = Input(Bool())
-            val credit = Output(Bool())
-        }
-    }
-}
-
-class SidebandNodeOuterIO (val params: SidebandParams) extends Bundle {
+class SidebandNodeIO(val sbParams: SidebandParams, val fdiParams: FdiParams)
+    extends Bundle {
+  // layers drive these
+  val inner = new Bundle {
+    val layer_to_node = Flipped(Decoupled(UInt(sbParams.sbNodeMsgWidth.W)))
+    /* This signal overrides the tx.ready and takes up the priority reserved
+     * queue slot */
+    // Should only be asserted high for access completion packets
+    val node_to_layer = Decoupled(UInt(sbParams.sbNodeMsgWidth.W))
+  }
+  // connect these to another sideband node
+  val outer = new Bundle {
     val tx = new Bundle {
-            val bits = Output(UInt(params.NC_width.W))
-            val valid = Output(Bool())
-            val credit = Input(Bool())
-        }
-        val rx = new Bundle {
-            val bits = Input(UInt(params.NC_width.W))
-            val valid = Input(Bool())
-            val credit = Output(Bool())
-        }
-}
-
-class SidebandLinkNodeOuterIO (val params: SidebandParams) extends Bundle {
-    val tx = new Bundle {
-        val bits = Output(UInt(params.NC_width.W))
-        val clock = Output(Bool())
+      val bits = Output(UInt(fdiParams.sbWidth.W))
+      val valid = Output(Bool())
+      val credit = Input(Bool())
     }
     val rx = new Bundle {
-        val bits = Input(UInt(params.NC_width.W))
-        val clock = Input(Bool())
+      val bits = Input(UInt(fdiParams.sbWidth.W))
+      val valid = Input(Bool())
+      val credit = Output(Bool())
     }
+  }
+}
+
+class SidebandNodeOuterIO(
+    val sbParams: SidebandParams,
+    val fdiParams: FdiParams,
+) extends Bundle {
+  val tx = new Bundle {
+    val bits = Output(UInt(fdiParams.sbWidth.W))
+    val valid = Output(Bool())
+    val credit = Input(Bool())
+  }
+  val rx = new Bundle {
+    val bits = Input(UInt(fdiParams.sbWidth.W))
+    val valid = Input(Bool())
+    val credit = Output(Bool())
+  }
+}
+
+class SidebandLinkNodeOuterIO(
+    val sbParams: SidebandParams,
+    val fdiParams: FdiParams,
+) extends Bundle {
+  val tx = new Bundle {
+    val bits = Output(UInt(fdiParams.sbWidth.W))
+    val clock = Output(Bool())
+  }
+  val rx = new Bundle {
+    val bits = Input(UInt(fdiParams.sbWidth.W))
+    val clock = Input(Bool())
+  }
 }
 
 // IO for the remote sideband
-class SidebandLinkIO (val params: SidebandParams) extends Bundle {
-    // layers drive these
-    val inner = new Bundle {
-        val layer_to_node = Flipped(Decoupled(UInt(params.MSG_width.W)))
-        // This signal overrides the tx.ready and takes up the priority reserved queue slot
-        // Should only be asserted high for access completion packets
-        val node_to_layer = Decoupled(UInt(params.MSG_width.W))
+class SidebandLinkIO(val sbParams: SidebandParams, val fdiParams: FdiParams)
+    extends Bundle {
+  // layers drive these
+  val inner = new Bundle {
+    val layer_to_node = Flipped(Decoupled(UInt(sbParams.sbNodeMsgWidth.W)))
+    /* This signal overrides the tx.ready and takes up the priority reserved
+     * queue slot */
+    // Should only be asserted high for access completion packets
+    val node_to_layer = Decoupled(UInt(sbParams.sbNodeMsgWidth.W))
+  }
+  // connect these to another sideband node
+  val outer = new Bundle {
+    val tx = new Bundle {
+      val bits = Output(UInt(fdiParams.sbWidth.W))
+      val clock = Output(Bool())
     }
-    // connect these to another sideband node
-    val outer = new Bundle {
-        val tx = new Bundle {
-            val bits = Output(UInt(params.NC_width.W))
-            val clock = Output(Bool())
-        }
-        val rx = new Bundle {
-            val bits = Input(UInt(params.NC_width.W))
-            val clock = Input(Bool())
-        }
+    val rx = new Bundle {
+      val bits = Input(UInt(fdiParams.sbWidth.W))
+      val clock = Input(Bool())
     }
+  }
 }
 
-class SidebandSwitcherIO (val params: SidebandParams) extends Bundle {
-    //layer drive these
-    val inner = Flipped(new SidebandSwitcherbundle(params))
-    // Sideband node drive these
-    val outer = new SidebandSwitcherbundle(params)
+class SidebandSwitcherIO(val sbParams: SidebandParams) extends Bundle {
+  // layer drive these
+  val inner = Flipped(new SidebandSwitcherbundle(sbParams))
+  // Sideband node drive these
+  val outer = new SidebandSwitcherbundle(sbParams)
 }
 
-class SidebandSwitcherbundle (val params: SidebandParams) extends Bundle {
-    val node_to_layer_above = Flipped(Decoupled(UInt(params.MSG_width.W)))
-    val layer_to_node_above = Decoupled(UInt(params.MSG_width.W))
-    val node_to_layer_below = Flipped(Decoupled(UInt(params.MSG_width.W)))
-    val layer_to_node_below = Decoupled(UInt(params.MSG_width.W))
+class SidebandSwitcherbundle(val sbParams: SidebandParams) extends Bundle {
+  val node_to_layer_above = Flipped(Decoupled(UInt(sbParams.sbNodeMsgWidth.W)))
+  val layer_to_node_above = Decoupled(UInt(sbParams.sbNodeMsgWidth.W))
+  val node_to_layer_below = Flipped(Decoupled(UInt(sbParams.sbNodeMsgWidth.W)))
+  val layer_to_node_below = Decoupled(UInt(sbParams.sbNodeMsgWidth.W))
 }
