e20cd216bef539dd96db42fd2c5846dcdc01da2e
Viansa Schmulbach <ansa@berkeley.edu>
2024-03-18 00:17:41 -0700

    lane tests + fixes

--- src/main/scala/logphy/Lanes.scala
+++ src/main/scala/logphy/Lanes.scala
@@ -3,6 +3,7 @@ package logphy
 
 import interfaces._
 import chisel3._
+import chisel3.util._
 import freechips.rocketchip.util.{AsyncQueue, AsyncQueueParams}
 
 class Lanes(
@@ -15,14 +16,18 @@ class Lanes(
   })
 
   val txMBFifo =
-    new AsyncQueue(
-      Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W)),
-      queueParams,
+    Module(
+      new AsyncQueue(
+        Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W)),
+        queueParams,
+      ),
     )
   val rxMBFifo =
-    new AsyncQueue(
-      Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W)),
-      queueParams,
+    Module(
+      new AsyncQueue(
+        Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W)),
+        queueParams,
+      ),
     )
 
   rxMBFifo.io.enq <> io.mainbandIo.rxData
@@ -41,17 +46,93 @@ class Lanes(
   assert(
     afeParams.mbSerializerRatio > 8 && afeParams.mbSerializerRatio % 8 == 0,
   )
+  val txDataVec = Wire(
+    Vec(afeParams.mbLanes, Vec(afeParams.mbSerializerRatio / 8, UInt(8.W))),
+  )
+  val ratioBytes = afeParams.mbSerializerRatio / 8
+  val rxDataVec = Wire(
+    Vec(ratioBytes, Vec(afeParams.mbLanes, UInt(8.W))),
+  )
   for (i <- 0 until afeParams.mbLanes) {
-    for (j <- 0 until afeParams.mbSerializerRatio / 8) {
-      txMBFifo.io.enq.bits(i) := io.mainbandLaneIO.txData.bits(
-        afeParams.mbLanes * 8 * j + (i * 8),
-        afeParams.mbLanes * 8 * j + (i * 8) + 8,
-      )
-      io.mainbandLaneIO.rxData
+    for (j <- 0 until ratioBytes) {
+      txDataVec(afeParams.mbLanes - 1 - i)(j) := io.mainbandLaneIO.txData
         .bits(
-          afeParams.mbLanes * 8 * j + i * 8,
-          afeParams.mbLanes * 8 * j + (i * 8) + 8,
-        ) := rxMBFifo.io.deq.bits(i)
+          afeParams.mbLanes * 8 * j + (i * 8) + 7,
+          afeParams.mbLanes * 8 * j + (i * 8),
+        )
+      rxDataVec(j)(afeParams.mbLanes - 1 - i) := rxMBFifo.io.deq
+        .bits(i)((j + 1) * 8 - 1, j * 8)
     }
+    txMBFifo.io.enq.bits(i) := txDataVec(i).asUInt
   }
+  io.mainbandLaneIO.rxData.bits := rxDataVec.asUInt
+  rxMBFifo.io.deq.ready := io.mainbandLaneIO.rxData.ready
+  io.mainbandLaneIO.txData.ready := txMBFifo.io.enq.ready
+}
+
+class MainbandSimIO(afeParams: AfeParams) extends Bundle {
+  val txData = Decoupled(
+    Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W)),
+  )
+  val rxData = Flipped(
+    Decoupled(Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W))),
+  )
+}
+
+class SimLanes(
+    afeParams: AfeParams,
+    queueParams: AsyncQueueParams,
+) extends Module {
+
+  val io = IO(new Bundle() {
+    val mainbandIo = new MainbandSimIO(afeParams)
+    val mainbandLaneIO = new MainbandLaneIO(afeParams)
+  })
+
+  val txMBFifo =
+    Module(
+      new Queue(
+        Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W)),
+        queueParams.depth,
+      ),
+    )
+  val rxMBFifo =
+    Module(
+      new Queue(
+        Vec(afeParams.mbLanes, Bits(afeParams.mbSerializerRatio.W)),
+        queueParams.depth,
+      ),
+    )
+
+  rxMBFifo.io.enq <> io.mainbandIo.rxData
+  txMBFifo.io.deq <> io.mainbandIo.txData
+
+  txMBFifo.io.enq.valid := io.mainbandLaneIO.txData.valid
+  io.mainbandLaneIO.rxData.valid := rxMBFifo.io.deq.valid
+  assert(
+    afeParams.mbSerializerRatio > 8 && afeParams.mbSerializerRatio % 8 == 0,
+  )
+  val txDataVec = Wire(
+    Vec(afeParams.mbLanes, Vec(afeParams.mbSerializerRatio / 8, UInt(8.W))),
+  )
+  val ratioBytes = afeParams.mbSerializerRatio / 8
+  val rxDataVec = Wire(
+    Vec(ratioBytes, Vec(afeParams.mbLanes, UInt(8.W))),
+  )
+  for (i <- 0 until afeParams.mbLanes) {
+    for (j <- 0 until ratioBytes) {
+      txDataVec(afeParams.mbLanes - 1 - i)(j) := io.mainbandLaneIO.txData
+        .bits(
+          afeParams.mbLanes * 8 * j + (i * 8) + 7,
+          afeParams.mbLanes * 8 * j + (i * 8),
+        )
+      rxDataVec(j)(afeParams.mbLanes - 1 - i) := rxMBFifo.io.deq
+        .bits(i)((j + 1) * 8 - 1, j * 8)
+    }
+    txMBFifo.io.enq.bits(i) := txDataVec(i).asUInt
+  }
+  io.mainbandLaneIO.rxData.bits := rxDataVec.asUInt
+  rxMBFifo.io.deq.ready := io.mainbandLaneIO.rxData.ready
+  io.mainbandLaneIO.txData.ready := txMBFifo.io.enq.ready
+
 }
--- src/test/scala/logphy/LogPhyLaneTest.scala
+++ src/test/scala/logphy/LogPhyLaneTest.scala
@@ -2,6 +2,7 @@ package edu.berkeley.cs.ucie.digital
 package logphy
 
 import chisel3._
+import chisel3.experimental.VecLiterals.AddObjectLiteralConstructor
 import chiseltest._
 import freechips.rocketchip.util.AsyncQueueParams
 import org.scalatest.flatspec.AnyFlatSpec
@@ -10,9 +11,76 @@ import interfaces._
 class LogPhyLaneTest extends AnyFlatSpec with ChiselScalatestTester {
   val afeParams = new AfeParams()
   val queueParams = new AsyncQueueParams()
-  behavior of "log phy lanes"
-  it should "correctly map bytes to their lanes" in {
-    test(new Lanes())
+  behavior of "log phy TX lanes"
+  it should "correctly map TX bytes to their lanes" in {
+    test(new SimLanes(afeParams, queueParams)) { c =>
+      c.io.mainbandLaneIO.txData.initSource()
+      c.io.mainbandLaneIO.txData.setSourceClock(c.clock)
+      c.io.mainbandIo.txData.initSink()
+      c.io.mainbandIo.txData.setSinkClock(c.clock)
+
+      c.io.mainbandLaneIO.txData.enqueueNow(
+        "h1234_5678_9abc_def0_0fed_cba9_8765_4321_1111_2222_3333_4444_5555_6666_7777_8888".U,
+      )
+      c.io.mainbandIo.txData
+        .expectDequeueNow(
+          Vec.Lit(
+            "h1211".U,
+            "h3411".U,
+            "h5622".U,
+            "h7822".U,
+            "h9a33".U,
+            "hbc33".U,
+            "hde44".U,
+            "hf044".U,
+            "h0f55".U,
+            "hed55".U,
+            "hcb66".U,
+            "ha966".U,
+            "h8777".U,
+            "h6577".U,
+            "h4388".U,
+            "h2188".U,
+          ),
+        )
+    }
+  }
+
+  behavior of "log phy RX lanes"
+  it should "correctly map RX bytes to their lanes" in {
+    test(new SimLanes(afeParams, queueParams)) { c =>
+      c.io.mainbandIo.rxData.initSource()
+      c.io.mainbandIo.rxData.setSourceClock(c.clock)
+      c.io.mainbandLaneIO.rxData.initSink()
+      c.io.mainbandLaneIO.rxData.setSinkClock(c.clock)
+
+      c.io.mainbandIo.rxData
+        .enqueueNow(
+          Vec.Lit(
+            "h1211".U,
+            "h3411".U,
+            "h5622".U,
+            "h7822".U,
+            "h9a33".U,
+            "hbc33".U,
+            "hde44".U,
+            "hf044".U,
+            "h0f55".U,
+            "hed55".U,
+            "hcb66".U,
+            "ha966".U,
+            "h8777".U,
+            "h6577".U,
+            "h4388".U,
+            "h2188".U,
+          ),
+        )
+
+      c.io.mainbandLaneIO.rxData.expectDequeueNow(
+        "h1234_5678_9abc_def0_0fed_cba9_8765_4321_1111_2222_3333_4444_5555_6666_7777_8888".U,
+      )
+
+    }
 
   }
 }
