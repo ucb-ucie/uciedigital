ac54c88f20e0bd2e5fefd905015b8187f23e2853
Viansa Schmulbach <ansa@berkeley.edu>
2024-04-19 14:45:51 -0700

    remove unused

--- src/test/scala/logphy/RdiBringupTest.scala
+++ src/test/scala/logphy/RdiBringupTest.scala
@@ -1,19 +1,17 @@
 package edu.berkeley.cs.ucie.digital
 package logphy
 
-import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class RdiBringupTest extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "RDI Bringup Module"
-  it should "correctly transition" in {
-    test(new RdiBringup) { c =>
-      c.io.sbTrainIO.msgReq.initSink().setSinkClock(c.clock)
-      c.io.sbTrainIO.msgReqStatus.initSource().setSourceClock(c.clock)
+  // behavior of "RDI Bringup Module"
+  // it should "correctly transition" in {
+  //   test(new RdiBringup) { c =>
+  //     c.io.sbTrainIO.msgReq.initSink().setSinkClock(c.clock)
+  //     c.io.sbTrainIO.msgReqStatus.initSource().setSourceClock(c.clock)
 
-
-    }
-  }
+  //   }
+  // }
 
 }
