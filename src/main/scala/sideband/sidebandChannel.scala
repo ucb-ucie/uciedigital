ad50bfdd93738817cd61d0d5033b9b7a8871d51e
Viansa Schmulbach <ansa@berkeley.edu>
2024-05-03 00:33:14 -0700

    update sb raw rx

--- src/main/scala/sideband/sidebandChannel.scala
+++ src/main/scala/sideband/sidebandChannel.scala
@@ -57,13 +57,17 @@ class PHYSidebandChannel(
     switcher.io.outer.layer_to_node_below.nodeq()
     lower_node.io.inner.layer_to_node <> io.inner.rawInput
   }
-  io.inner.switcherBundle <> switcher.io.inner
+
   when(io.inner.rxMode === RXTXMode.RAW) {
     switcher.io.outer.node_to_layer_below.noenq()
     lower_node.io.inner.node_to_layer <> io.inner.switcherBundle.node_to_layer_below
+    switcher.io.inner.layer_to_node_below <> io.inner.switcherBundle.layer_to_node_below
+    switcher.io.inner.node_to_layer_above <> io.inner.switcherBundle.node_to_layer_above
+    switcher.io.inner.layer_to_node_above <> io.inner.switcherBundle.layer_to_node_above
 
   }.otherwise {
     lower_node.io.inner.node_to_layer <> switcher.io.outer.node_to_layer_below
+    io.inner.switcherBundle <> switcher.io.inner
   }
   lower_node.io.rxMode := io.inner.rxMode
 
