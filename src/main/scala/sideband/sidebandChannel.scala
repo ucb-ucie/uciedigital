e545d8e34abbf31f31efb12b72f75414f552ff58
Viansa Schmulbach <ansa@berkeley.edu>
2024-05-03 01:02:06 -0700

    update sb raw rx

--- src/main/scala/sideband/sidebandChannel.scala
+++ src/main/scala/sideband/sidebandChannel.scala
@@ -60,6 +60,8 @@ class PHYSidebandChannel(
 
   when(io.inner.rxMode === RXTXMode.RAW) {
     switcher.io.outer.node_to_layer_below.noenq()
+    switcher.io.inner.node_to_layer_below.nodeq()
+
     lower_node.io.inner.node_to_layer <> io.inner.switcherBundle.node_to_layer_below
     switcher.io.inner.layer_to_node_below <> io.inner.switcherBundle.layer_to_node_below
     switcher.io.inner.node_to_layer_above <> io.inner.switcherBundle.node_to_layer_above
